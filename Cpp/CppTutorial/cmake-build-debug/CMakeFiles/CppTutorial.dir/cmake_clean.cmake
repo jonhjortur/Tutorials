af1c26616091e64fe8b0855d8db7d7b87643fc85
Jón Hjörtur <jonhjortur@protonmail.com>
2020-05-09 00:09:13 +0000

    File structure

---
+++ Cpp/CppTutorial/cmake-build-debug/CMakeFiles/CppTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppTutorial.dir/main.cpp.o"
+  "CppTutorial"
+  "CppTutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
